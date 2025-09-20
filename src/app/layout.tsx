dc5a9b0be3f269766420533e0d658349f509d029
Mahfuj-8262 <mafu522953@gmail.com>
2025-09-21 00:28:55 +0600

    Adding the server sleep alert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link href="https://fonts.googleapis.com/css2?family=Golos+Text:wght@400..900&display=swap" rel="stylesheet"></link>
       </head>
       <body className="bg-gray-100">
+        <div className="bg-yellow-100 text-yellow-800 border-b border-yellow-300 text-center p-2 text-sm">
+          ⚠️ Heads up: This app is hosted on Azure Free Student Plan. 
+          So the server “sleeps” after 20 minutes of inactivity.
+          If a request fails the first time, please retry after a few seconds.
+        </div>
         <AuthProvider>
           <Header />
           <main className="bg-gradient-to-br from-zinc-700 via-zinc-500 to-zinc-400" style={{
